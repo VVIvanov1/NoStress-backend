dd4682bfe84ceae24bd28700d99716b4f4db69ef
VVIvanov1 <ivanovv.tca@gmail.com>
2022-06-15 00:25:45 +0600

    initial back-end with registration and login functionality

--- client/src/components/Header/ResetPasswordPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ResetPasswordPage = () => {
-  return <div>password reset page</div>;
-};
-
-export default ResetPasswordPage;
--- client/src/components/tooltip/tooltipPosition.js
+++
@@ -1,8 +0,0 @@
-export const getElementPosition = (element) => {
-  let parentRect = element.getBoundingClientRect();
-  let tooltipRectHalf = 75;
-  let left = parentRect.left + (parentRect.width / 2 - tooltipRectHalf);
-  let top = parentRect.bottom;
-
-  return { top: `${top}px`, left: `${left}px` };
-};
