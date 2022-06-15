ebf4d1d268d4918672db1f925455864312789c02
VVIvanov1 <ivanovv.tca@gmail.com>
2022-06-15 07:59:31 +0600

    heroku setup prepared

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 const path = require("path");
 // const mongoose = require("mongoose");
+const connUrl = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@backofice.v7bq8ry.mongodb.net/?retryWrites=true&w=majority`;
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(connUrl);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
