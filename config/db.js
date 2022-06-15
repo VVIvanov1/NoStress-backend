49b42bdc1ec4cab7e2f75dd098c099e772cb8bd5
VVIvanov1 <ivanovv.tca@gmail.com>
2022-06-15 08:34:06 +0600

    issues with mongoose connection

--- config/db.js
+++ config/db.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
 const path = require("path");
 // const mongoose = require("mongoose");
-const connUrl = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@backofice.v7bq8ry.mongodb.net/?retryWrites=true&w=majority`;
+const connUrl = `mongodb+srv://admin:${process.env.MONGO_PASSWORD}@backofice.v7bq8ry.mongodb.net/?retryWrites=true&w=majority`;
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
-
+const options = {
+  autoIndex: false, // Don't build indexes
+  maxPoolSize: 10, // Maintain up to 10 socket connections
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4, // Use IPv4, skip trying IPv6
+};
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(connUrl);
+    const conn = await mongoose.connect(connUrl, options);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
