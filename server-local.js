4f02c71e0598859758c8783b71508df6bb4bd8e5
VVIvanov1 <ivanovv.tca@gmail.com>
2022-06-15 08:39:18 +0600

    issues with mongoose connection

--- server-local.js
+++ server-local.js
@@ -2,4 +2,6 @@
 
 const app = require("./src/api");
 
-app.listen(5000, () => console.log("Local app listening on port 5000!"));
+app.listen(process.env.PORT || 5000, () =>
+  console.log("Local app listening on port 5000!")
+);
