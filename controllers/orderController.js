437f84409e06cf1414e84cf176e6abda4a68a26d
VVIvanov1 <ivanovv.tca@gmail.com>
2022-06-17 21:22:44 +0600

    refined myOrders qpi endpoint

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -53,6 +53,7 @@ const getMyOrders = asyncHandler(async (req, res) => {
 const newOrderWeb = asyncHandler(async (req, res) => {
   try {
     let ress = await saveNewWebOrder(req.body);
+    let lead = await saveNewLead({ order: req.body, orderId: ress._id });
     res.status(200).json(req.body);
   } catch (error) {
     console.log(error);
@@ -73,12 +74,14 @@ const newOrderManual = asyncHandler(async (req, res) => {
 });
 
 const saveNewWebOrder = asyncHandler(async (req, res) => {
-  try {
-    let newOrder = await saveNewWeb(req.body);
-    res.status(200).json(newOrder);
-  } catch (error) {
-    console.error(error);
-  }
+  console.log(req.body);
+  res.status(200).json({ message: "ok" });
+  // try {
+  //   let newOrder = await saveNewWeb(req.body);
+  //   res.status(200).json(newOrder);
+  // } catch (error) {
+  //   console.error(error);
+  // }
 });
 async function saveNewWeb(obj) {
   console.log(obj);
